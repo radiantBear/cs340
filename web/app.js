a0fcd6c0b356ccda4caabf3c1cb9e4bd5bb477d0
Nate Baird <85893453+radiantBear@users.noreply.github.com>
2025-05-06 22:35:49 -0700

    Add simple Read tables

--- web/app.js
+++ web/app.js
@@ -1,17 +1,29 @@
 require('dotenv').config();
 const app = require('express')();
 const port = process.env.PORT || 3000;
+const db = require('./database/db-connector');
 
-app.engine('handlebars', require('express-handlebars').engine({ defaultLayout: 'main' }));
-app.set('view engine', 'handlebars');
+app.engine('.hbs', require('express-handlebars').engine({ defaultLayout: 'main', extname: '.hbs' }));
+app.set('view engine', '.hbs');
 
 
 app.get('/', (_, res) => {
     res.status(200).render('index');
 });
 
-app.get('*', (_, res) => {
-    res.status(404).render('404');
+app.get('/employees', async (_, res) => {
+    const [employees] = await db.execute('SELECT * FROM Employees;');
+    res.status(200).render('employees', { employees });
+});
+
+app.get('/services', async (_, res) => {
+    const [services] = await db.execute('SELECT * FROM Services;');
+    res.status(200).render('services', { services });
+});
+
+app.get('/customers', async (_, res) => {
+    const [customers] = await db.execute('SELECT * FROM Customers;');
+    res.status(200).render('customers', { customers });
 });
 
 
