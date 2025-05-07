91cfa2136d46d892395bb426b1e587e5ed207bf7
Nate Baird <85893453+radiantBear@users.noreply.github.com>
2025-05-06 23:00:06 -0700

    Begin appointment input template

--- web/app.js
+++ web/app.js
@@ -26,6 +26,23 @@ app.get('/customers', async (_, res) => {
     res.status(200).render('customers', { customers });
 });
 
+app.get('/appointments/new', async (_, res) => {
+    const [employees] = await db.execute('SELECT * FROM Employees;');
+    const [customers] = await db.execute('SELECT * FROM Customers;');
+    const [services] = await db.execute('SELECT * FROM Services;');
+
+    res.status(200).render('appointment_input', { employees, customers, services, newAppointment: true });
+});
+
+app.get('/appointments/:id', async (req, res) => {
+    const [employees] = await db.execute('SELECT * FROM Employees;');
+    const [customers] = await db.execute('SELECT * FROM Customers;');
+    const [services] = await db.execute('SELECT * FROM Services;');
+
+    const [[appointment]] = await db.execute('SELECT * FROM Appointments WHERE appointmentId = ?;', [req.params.id]);
+    console.log(appointment);
+    res.status(200).render('appointment_input', { employees, customers, services, appointment, newAppointment: false });
+});
 
 app.listen(port, () => {
     console.log("Server is listening on port", port);
