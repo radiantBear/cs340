b0590ceec9aa0fdbc1b59a976faa4f0f09652fac
Carlos Alvarado <165974612+alvarac2@users.noreply.github.com>
2025-05-22 01:47:52 -0500

    Replaced DDL/DML files and added PL/SQL file. Added CUD routes in app.js (#7)
    * Added CRUD sql file and appointments and invoices pages
    
    * Added navbar to main layout, updated index page and light bootstrap styling
    
    * Replaced ddl and dml files and added PL.sql file
    
    * Added routes for CUD calling stored procedures

---
+++ PL.sql
@@ -0,0 +1,74 @@
+-- #############################
+-- CREATE appointmentServices
+-- #############################
+DROP PROCEDURE IF EXISTS sp_InsertAppointmentService;
+DELIMITER //
+CREATE PROCEDURE sp_InsertAppointmentService (
+    IN p_serviceId INT,
+    IN p_appointmentId INT,
+    OUT p_resultStatus INT
+)
+BEGIN
+	INSERT INTO appointmentservices (serviceId, appointmentId)
+	VALUES (p_serviceId, p_appointmentId);
+	IF ROW_COUNT() > 0 THEN
+		SET p_resultStatus = 1;
+	ELSE SET p_resultStatus = 0;
+    END IF;
+END //
+DELIMITER ;
+
+-- #############################
+-- UPDATE appointmentServices
+-- #############################
+DROP PROCEDURE IF EXISTS sp_UpdateAppointmentService;
+DELIMITER //
+CREATE PROCEDURE sp_UpdateAppointmentService(
+    IN p_oldServiceId INT,
+	IN p_newServiceId INT,
+    IN p_appointmentId INT,
+    OUT p_resultStatus INT
+)
+BEGIN
+    UPDATE appointmentservices
+		SET serviceId = p_newServiceId
+			WHERE appointmentId = p_appointmentId
+			AND serviceId = p_oldServiceId; 
+END //
+DELIMITER ;
+
+-- #############################
+-- DELETE AppointmentServices
+-- #############################
+DROP PROCEDURE IF EXISTS sp_DeleteAppointmentService;
+DELIMITER //
+CREATE PROCEDURE sp_DeleteAppointmentService(
+	IN p_appointmentId INT,
+	IN p_serviceId INT,
+    OUT p_resultStatus INT
+)
+BEGIN
+    DECLARE error_message VARCHAR(255); 
+    DECLARE EXIT HANDLER FOR SQLEXCEPTION
+    BEGIN
+        ROLLBACK;
+        RESIGNAL;
+    END;
+
+    START TRANSACTION;
+        DELETE FROM appointmentservices
+			WHERE appointmentId = p_appointmentId
+            AND serviceId = p_serviceId;
+
+        IF ROW_COUNT() = 1 THEN
+			SET p_resultStatus = 1;
+		ELSE 
+			SET p_resultStatus = 0;
+            SET error_message = CONCAT('No matching record found in appointmentservices for ids (appointment, service): ', p_appointmentId, p_serviceId);
+            SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = error_message;
+        END IF;
+
+    COMMIT;
+
+END //
+DELIMITER ;
--- ddl.sql
+++ ddl.sql
@@ -1,117 +1,197 @@
-CREATE TABLE Employees (
-    employeeId int PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(255) NOT NULL,
-    phone CHAR(10) NOT NULL,
-    email VARCHAR(255) NOT NULL
-);
+DROP PROCEDURE IF EXISTS sp_LoadGreenscapesDb;
+DELIMITER //
+CREATE PROCEDURE sp_LoadGreenscapesDb()
+BEGIN
+	SET FOREIGN_KEY_CHECKS=0;
+    
+	DROP TABLE IF EXISTS Employees;
+	CREATE TABLE Employees (
+		employeeId int PRIMARY KEY AUTO_INCREMENT,
+		name VARCHAR(255) NOT NULL,
+		phone CHAR(10) NOT NULL,
+		email VARCHAR(255) NOT NULL
+	);
+	
+	DROP TABLE IF EXISTS Services;
+	CREATE TABLE Services (
+		serviceId int PRIMARY KEY AUTO_INCREMENT,
+		name VARCHAR(255) NOT NULL,
+		description VARCHAR(255) NOT NULL,
+		price DECIMAL(10, 2) NOT NULL
+	);
+	
+	DROP TABLE IF EXISTS Customers;
+	CREATE TABLE Customers (
+		customerId int PRIMARY KEY AUTO_INCREMENT,
+		name VARCHAR(255) NOT NULL,
+		phone CHAR(10) NOT NULL,
+		email VARCHAR(255) NOT NULL,
+		street VARCHAR(255) NOT NULL,
+		city VARCHAR(255) NOT NULL,
+		state VARCHAR(255) NOT NULL,
+		zipCode int NOT NULL,
+		currentCustomer BOOLEAN NOT NULL
+	);
+	
+	DROP TABLE IF EXISTS Appointments;
+	CREATE TABLE Appointments (
+		appointmentId int PRIMARY KEY AUTO_INCREMENT,
+		employeeId int NOT NULL,
+		customerId int NOT NULL,
+		appointmentDate DATE NOT NULL,
+		status VARCHAR(255) NOT NULL,
+		notes TEXT NULL,
+		CONSTRAINT `fk-customerId`
+			FOREIGN KEY (customerId)
+            REFERENCES Customers(customerId)
+            ON DELETE CASCADE,
+		CONSTRAINT `fk-employeeId`
+			FOREIGN KEY (employeeId)
+            REFERENCES Employees(employeeId)
+            ON DELETE CASCADE
+	);
+	
+	DROP TABLE IF EXISTS AppointmentServices;
+	CREATE TABLE AppointmentServices (
+		serviceId int NOT NULL,
+		appointmentId int NOT NULL,
+	
+		PRIMARY KEY (serviceId, appointmentId),
+        CONSTRAINT `fk-appointmentId`
+			FOREIGN KEY (appointmentId)
+            REFERENCES Appointments(appointmentId)
+            ON DELETE CASCADE,
+		CONSTRAINT `fk-serviceId`
+			FOREIGN KEY (serviceId)
+            REFERENCES Services(serviceId)
+            ON DELETE CASCADE
+	);
+	
+	DROP TABLE IF EXISTS Invoices;
+	CREATE TABLE Invoices (
+		invoiceId int PRIMARY KEY,
+		date DATE NOT NULL,
+		totalCost DECIMAL(10, 2) NOT NULL,
+		elapsedTime TIME NOT NULL,
+        
+		CONSTRAINT `fk-invoiceId`
+			FOREIGN KEY (invoiceId)
+			REFERENCES Appointments(appointmentId)
+			ON DELETE CASCADE
+	);
+	
+	-- SAMPLE DATA --
+    
+	INSERT INTO Employees (
+		name, phone, email
+	)
+	VALUES 
+	("Jack Johnson", "1111111111", "jack.johnson@fake.com"),
+	("Emilio Riveras", "2222222222", "emilio.riveras@fake.com"),
+	("Patrick Fisher", "3333333333", "patrick.fisher@fake.com"),
+	("Juan Alvarez", "4444444444", "juan.alvarez@fake.com"),
+	("Jacob Holden", "5555555555", "jacob.holden@fake.com");
+	
+	INSERT INTO Customers (
+		name, phone, email, street, city, state, zipCode, currentCustomer
+	)
+	VALUES 
+	("Julie Morales", "6666666666", "julie.morales@fake.com", "1111 first street", "Mckinney", "Texas", 11111, 1),
+	("Jose Vasquez", "7777777777", "jose.vasquez@fake.com", "2222 second street", "Allen", "Texas", 22222, 0),
+	("Karen Blair", "8888888888", "karen.blair@fake.com", "3333 third street", "Plano", "Texas", 33333, 1),
+	("Maria Gomez", "9999999999", "maria.gomez@fake.com", "4444 fourth street", "Dallas", "Texas", 44444, 0),
+	("John Baker", "1234567890", "john.baker@fake.com", "5555 fifth street", "Mckinney", "Texas", 11111, 1);
+	
+	INSERT INTO Services (
+		name, description, price
+	)
+	VALUES 
+	("Lawn Mowing", "Mowing front and back yard", "50"),
+	("Tree Pruning", "Shaping trees with careful pruning", "55"),
+	("Weed Control", "Applying herbicides lawns to keep weeds at bay", "45"),
+	("Lawn Fertilizing", "Applying fertilizer to lawns to promote growth", "40"),
+	("Garden Bed Clean Up", "Removing dead plants, leaves and weeds and applying a new layer of mulch", "30");
+	
+	INSERT INTO Appointments (
+		employeeId, customerId, appointmentDate, status, notes
+	)
+	VALUES
+	(
+		(SELECT employeeId FROM Employees WHERE name = 'Jack Johnson'),
+		(SELECT customerId FROM Customers WHERE name = 'Jose Vasquez'),
+		'2025-01-25', 'Completed', 'Customer was very happy'
+	),
+	(
+		(SELECT employeeId FROM Employees WHERE name = 'Emilio Riveras'),
+		(SELECT customerId FROM Customers WHERE name = 'Karen Blair'),
+		'2025-02-11', 'Completed', 'Customer was dissatisfied'
+	),
+	(
+		(SELECT employeeId FROM Employees WHERE name = 'Juan Alvarez'),
+		(SELECT customerId FROM Customers WHERE name = 'John Baker'),
+		'2025-03-13', 'Completed', NULL
+	),
+	(
+		(SELECT employeeId FROM Employees WHERE name = 'Patrick Fisher'),
+		(SELECT customerId FROM Customers WHERE name = 'Maria Gomez'),
+		'2025-05-30', 'Scheduled', NULL
+	),
+	(
+		(SELECT employeeId FROM Employees WHERE name = 'Emilio Riveras'),
+		(SELECT customerId FROM Customers WHERE name = 'Julie Morales'),
+		'2025-03-23', 'Canceled', 'Customer went on vacation'
+	);
+	
+	INSERT INTO AppointmentServices (
+		appointmentId, serviceId
+	)
+	VALUES 
+	(
+		(SELECT A.appointmentId
+		FROM Appointments A
+		JOIN Customers C ON A.customerId = C.customerId
+		WHERE C.name = 'Jose Vasquez' AND A.appointmentDate = '2025-01-25'),
+		(SELECT serviceId FROM Services WHERE name = 'Lawn Mowing')
+	),
+	(
+		(SELECT A.appointmentId
+		FROM Appointments A
+		JOIN Customers C ON A.customerId = C.customerId
+		WHERE C.name = 'Jose Vasquez' AND A.appointmentDate = '2025-01-25'),
+		(SELECT serviceId FROM Services WHERE name = 'Lawn Fertilizing')
+	),
+	(
+		(SELECT A.appointmentId
+		FROM Appointments A
+		JOIN Customers C ON A.customerId = C.customerId
+		WHERE C.name = 'Karen Blair' AND A.appointmentDate = '2025-02-11'),
+		(SELECT serviceId FROM Services WHERE name = 'Tree Pruning')
+	),
+	(
+		(SELECT A.appointmentId
+		FROM Appointments A
+		JOIN Customers C ON A.customerId = C.customerId
+		WHERE C.name = 'John Baker' AND A.appointmentDate = '2025-03-13'),
+		(SELECT serviceId FROM Services WHERE name = 'Garden Bed Clean Up')
+	),
+	(
+		(SELECT A.appointmentId
+		FROM Appointments A
+		JOIN Customers C ON A.customerId = C.customerId
+		WHERE C.name = 'Maria Gomez' AND A.appointmentDate = '2025-05-30'),
+		(SELECT serviceId FROM Services WHERE name = 'Weed Control')
+	);
+	
+	
+	INSERT INTO Invoices (
+		invoiceId, date, totalCost, elapsedTime
+	)
+	VALUES 
+	(1, "2025-01-25", "90", "01:30:00"),
+	(2, "2025-02-11", "55", "00:45:00"),
+	(3, "2025-03-13", "30", "00:33:00");
+	
+    SET FOREIGN_KEY_CHECKS=1;
+END //
 
-CREATE TABLE Services (
-    serviceId int PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(255) NOT NULL,
-    description VARCHAR(255) NOT NULL,
-    price DECIMAL(10, 2) NOT NULL
-);
-
-CREATE TABLE Customers (
-    customerId int PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(255) NOT NULL,
-    phone CHAR(10) NOT NULL,
-    email VARCHAR(255) NOT NULL,
-    street VARCHAR(255) NOT NULL,
-    city VARCHAR(255) NOT NULL,
-    state VARCHAR(255) NOT NULL,
-    zipCode int NOT NULL,
-    currentCustomer BOOLEAN NOT NULL
-);
-
-CREATE TABLE Appointments (
-    appointmentId int PRIMARY KEY AUTO_INCREMENT,
-    employeeId int NOT NULL,
-    customerId int NOT NULL,
-    appointmentDate DATE NOT NULL,
-    status VARCHAR(255) NOT NULL,
-    notes TEXT NULL,
-
-    FOREIGN KEY (customerId) REFERENCES Customers(customerId),
-    FOREIGN KEY (employeeId) REFERENCES Employees(employeeId)
-);
-
-CREATE TABLE AppointmentServices (
-    serviceId int NOT NULL,
-    appointmentId int NOT NULL,
-
-    PRIMARY KEY (serviceId, appointmentId),
-    FOREIGN KEY (appointmentId) REFERENCES Appointments(appointmentId),
-    FOREIGN KEY (serviceId) REFERENCES Services(serviceId)
-);
-
-CREATE TABLE Invoices (
-    invoiceId int PRIMARY KEY,
-    date DATE NOT NULL,
-    totalCost DECIMAL(10, 2) NOT NULL,
-    elapsedTime TIME NOT NULL,
-
-    FOREIGN KEY (invoiceId) REFERENCES Appointments(appointmentId)
-);
-
-
--- ------------------------------------ Sample Data --------------------------------------
-
-INSERT INTO Employees (
-    name, phone, email
-)
-VALUES 
-("Jack Johnson", "1111111111", "jack.johnson@fake.com"),
-("Emilio Riveras", "2222222222", "emilio.riveras@fake.com"),
-("Patrick Fisher", "3333333333", "patrick.fisher@fake.com"),
-("Juan Alvarez", "4444444444", "juan.alvarez@fake.com"),
-("Jacob Holden", "5555555555", "jacob.holden@fake.com");
-
-INSERT INTO Customers (
-    name, phone, email, street, city, state, zipCode, currentCustomer
-)
-VALUES 
-("Julie Morales", "6666666666", "julie.morales@fake.com", "1111 first street", "Mckinney", "Texas", 11111, 1),
-("Jose Vasquez", "7777777777", "jose.vasquez@fake.com", "2222 second street", "Allen", "Texas", 22222, 0),
-("Karen Blair", "8888888888", "karen.blair@fake.com", "3333 third street", "Plano", "Texas", 33333, 1),
-("Maria Gomez", "9999999999", "maria.gomez@fake.com", "4444 fourth street", "Mckinney", "Texas", 44444, 0),
-("John Baker", "1234567890", "john.baker@fake.com", "5555 fifth street", "Mckinney", "Texas", 55555, 1);
-
-INSERT INTO Services (
-    name, description, price
-)
-VALUES 
-("Lawn Mowing", "Mowing front and back yard", "50"),
-("Tree Pruning", "Shaping trees with careful pruning", "55"),
-("Weed Control", "Applying herbicides lawns to keep weeds at bay", "45"),
-("Lawn Fertilizing", "Applying fertilizer to lawns to promote growth", "40"),
-("Garden Bed Clean Up", "Removing dead plants, leaves and weeds and applying a new layer of mulch", "30");
-
-INSERT INTO Appointments (
-    employeeId, customerId, appointmentDate, status, notes
-)
-VALUES 
-(1, 2, "2025-01-25", "Completed", "Customer was very happy"),
-(2, 3, "2025-02-11", "Completed", "Customer was dissatisfied"),
-(4, 5, "2025-03-13", "Completed", NULL),
-(3, 4, "2025-05-30", "Scheduled", NULL),
-(2, 1, "2025-03-23", "Canceled", "Customer went on vacation");
-
-INSERT INTO AppointmentServices (
-    appointmentId, serviceId
-)
-VALUES 
-(1, 1),
-(1, 4),
-(2, 2),
-(3, 5),
-(4, 3);
-
-
-INSERT INTO Invoices (
-    invoiceId, date, totalCost, elapsedTime
-)
-VALUES 
-(1, "2025-01-25", "90", "01:30:00"),
-(2, "2025-02-11", "55", "00:45:00"),
-(3, "2025-03-13", "30", "00:33:00");
+DELIMITER ;
