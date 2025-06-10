083e153bcb81ae0af307b8f821301428b68dc827
Carlos Alvarado <alvarac2@oregonstate.edu>
2025-06-10 00:06:23 -0500

    Added more comments

--- web/app.js
+++ web/app.js
@@ -1,9 +1,12 @@
+// Application logic javascript file
+
 // Citation for the following functions:
 // Date: 05/20/2025
 // Adapted from Exploration - Web Application Technology and Exploration - Implementing CUD operations in your app
 // Source URL: https://oregonstate.instructure.com/courses/1999601/pages/exploration-implementing-cud-operations-in-your-app?module_item_id=25352968
 // Source URL: https://oregonstate.instructure.com/courses/1999601/pages/exploration-web-application-technology-2
 
+// Initial setup
 require('dotenv').config();
 const express = require('express');
 const app = express();
@@ -26,6 +29,7 @@ Handlebars.registerHelper('eq', (a, b) => {
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Routes
 app.get('/', handle.getRootHTML);
 app.get('/employees', handle.getEmployeesHTML);
 app.get('/services', handle.getServicesHTML);
@@ -39,105 +43,9 @@ app.post('/resetDb', handle.resetDb);
 
 app.post('/appointments/:appointmentId/service', handle.addAppointmentService);
 app.patch('/appointments/:appointmentId/service/:oldServiceId', handle.updateAppointmentService);
-app.delete('/appointments/:appointmentId/services/:serviceId', );
+app.delete('/appointments/:appointmentId/services/:serviceId', handle.deleteAppointmentService);
 
 app.use('/static', express.static('static'));
-
-app.post('/resetDb', async (req, res) => {
-  try {
-    await db.query('CALL sp_LoadGreenscapesDb();');
-    console.log('Database was reset');
-
-    res.redirect('/');
-  } catch (error) {
-    console.error('Error resetting database:', error);
-    res.status(500).send('Failed to reset database.');
-  }
-});
-
-app.post('/appointments/:appointmentId/addservice', async function (req, res) {
-    try {
-        const { appointmentId } = req.params;
-        let data = req.body;
-
-        if (isNaN(parseInt(data.newServiceId)))
-            data.newServiceId = null;
-
-        const query1 = `CALL sp_InsertAppointmentService(?, ?, @resultStatus);`;
-
-        const [[[rows]]] = await db.query(query1, [
-            data.newServiceId,
-            appointmentId
-        ]);
-
-        if (rows.resultStatus == 1)
-            console.log(`Service was successfully added `
-            );
-        else console.log(`Service failed to be added`);
-
-        res.redirect(`/appointments/${appointmentId}`);
-    } catch (error) {
-        console.error('Error executing queries:', error);
-        res.status(500).send(
-            'An error occurred while executing the database queries.'
-        );
-    }
-});
-
-app.post('/appointments/:appointmentId/updateservice/:oldServiceId', async function (req, res) {
-    try {
-        const { appointmentId, oldServiceId } = req.params;
-        let data = req.body;
-
-        if (isNaN(parseInt(data.newServiceId)))
-            data.newServiceId = null;
-
-        const query1 = 'CALL sp_UpdateAppointmentService(?, ?, ?, @resultStatus);';
-        const [[[rows]]] = await db.query(query1, [
-            oldServiceId,
-            data.newServiceId,
-            appointmentId
-        ]);
-        
-        if (rows.resultStatus == 1)
-            console.log(`Service was successfully updated `
-            );
-        else console.log(`Service failed to be updated`);
-
-        res.redirect(`/appointments/${appointmentId}`);
-    } catch (error) {
-        console.error('Error executing queries:', error);
-        res.status(500).send(
-            'An error occurred while executing the database queries.'
-        );
-    }
-});
-
-app.post('/appointments/:appointmentId/deleteservice/:serviceId', async function (req, res) {
-    try {
-        const { appointmentId, serviceId } = req.params;
-
-        const query1 = `CALL sp_DeleteAppointmentService(?, ?, @resultStatus);`;
-        const [[[rows]]] = await db.query(query1, [
-            appointmentId,
-            serviceId
-        ]);
-
-        if (rows.resultStatus == 1)
-            console.log(`Service was successfully deleted `
-            );
-        else console.log(`Service failed to be deleted`);
-
-        // Redirect the user to the updated webpage data
-        res.redirect(`/appointments/${appointmentId}`);
-    } catch (error) {
-        console.error('Error executing queries:', error);
-        // Send a generic error message to the browser
-        res.status(500).send(
-            'An error occurred while executing the database queries.'
-        );
-    }
-});
 
 app.listen(port, () => {
     console.log("Server is listening on port", port);
--- web/database/db-connector.js
+++ web/database/db-connector.js
@@ -1,3 +1,5 @@
+// Javascript file that establishes a connection to the database
+
 // Citation for the following code block:
 // Date: 05/20/2025
 // Adapted from Exploration - Web Application Technology and Exploration
