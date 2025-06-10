e241b36a312bf2f51bd0cc487aa76b9a3540d3e9
Carlos Alvarado <alvarac2@oregonstate.edu>
2025-06-09 23:21:01 -0500

    Added comments to all files EXCEPT app.js

--- web/database/db-connector.js
+++ web/database/db-connector.js
@@ -1,3 +1,8 @@
+// Citation for the following code block:
+// Date: 05/20/2025
+// Adapted from Exploration - Web Application Technology and Exploration
+// Source URL: https://oregonstate.instructure.com/courses/1999601/pages/exploration-web-application-technology-2
+
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
