9bfbea248c519a91d9e09c335910cf6b514b7a4e
Nate Baird <85893453+radiantBear@users.noreply.github.com>
2025-06-09 21:22:17 -0700

    Refactor server-side code (#11)
    * Clean up table descriptions
    
    * Extract route handlers from Handlebars setup

--- web/app.js
+++ web/app.js
@@ -9,6 +9,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 const db = require('./database/db-connector');
+const handle = require('./handlers');
 const Handlebars = require('handlebars');
 
 app.engine('.hbs', require('express-handlebars').engine({ defaultLayout: 'main', extname: '.hbs' }));
@@ -25,161 +26,20 @@ Handlebars.registerHelper('eq', (a, b) => {
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get('/', (_, res) => {
-    res.status(200).render('index');
-});
+app.get('/', handle.getRootHTML);
+app.get('/employees', handle.getEmployeesHTML);
+app.get('/services', handle.getServicesHTML);
+app.get('/customers', handle.getCustomersHTML);
+app.get('/invoices', handle.getInvoicesHTML);
+app.get('/appointments', handle.getAppointmentsHTML);
+app.get('/appointments/new', handle.getNewAppointmentHTML);
+app.get('/appointments/:id', handle.getEditAppointmentHTML);
 
-app.get('/employees', async (_, res) => {
-    const [employees] = await db.execute('SELECT * FROM Employees;');
-    res.status(200).render('employees', { employees });
-});
+app.post('/resetDb', handle.resetDb);
 
-app.get('/services', async (_, res) => {
-    const [services] = await db.execute('SELECT * FROM Services;');
-    res.status(200).render('services', { services });
-});
-
-app.get('/customers', async (_, res) => {
-    const [customers] = await db.execute('SELECT * FROM Customers;');
-    res.status(200).render('customers', { customers });
-});
-
-app.get('/invoices', async (_, res) => {
-    const [invoices] = await db.execute('SELECT * FROM Invoices;');
-    res.status(200).render('invoices', { invoices });
-});
-
-app.get('/appointments', async (_, res) => {
-    const [appointments] = await db.execute(`
-        SELECT 
-            a.appointmentId,
-            e.name AS employeeName,
-            c.name AS customerName,
-            a.appointmentDate,
-            a.status,
-            a.notes,
-            GROUP_CONCAT(s.name ORDER BY s.name SEPARATOR ', ') AS servicesRequested
-        FROM Appointments a
-            JOIN Employees e ON a.employeeId = e.employeeId
-            JOIN Customers c ON a.customerId = c.customerId
-            LEFT JOIN AppointmentServices aps ON a.appointmentId = aps.appointmentId
-            LEFT JOIN Services s ON aps.serviceId = s.serviceId
-        GROUP BY a.appointmentId, e.name, c.name, a.appointmentDate, a.status, a.notes
-        ORDER BY a.appointmentDate ASC;
-    `);
-
-    res.status(200).render('appointments', { appointments });
-});
-
-app.get('/appointments/new', async (_, res) => {
-    const [employees] = await db.execute('SELECT * FROM Employees;');
-    const [customers] = await db.execute('SELECT * FROM Customers;');
-    const [services] = await db.execute('SELECT * FROM Services;');
-
-    res.status(200).render('appointment_input', { employees, customers, services, newAppointment: true });
-});
-
-app.get('/appointments/:id', async (req, res) => {
-    const [employees] = await db.execute('SELECT * FROM Employees;');
-    const [customers] = await db.execute('SELECT * FROM Customers;');
-    const [services] = await db.execute('SELECT * FROM Services;');
-
-    const [[appointment]] = await db.execute('SELECT * FROM Appointments WHERE appointmentId = ?;', [req.params.id]);
-    const [selectedServices] = await db.execute('SELECT serviceId FROM AppointmentServices WHERE appointmentId = ?;', [req.params.id]);
-    res.status(200).render('appointment_input', { employees, customers, services, appointment, selectedServices, newAppointment: false });
-});
-
-app.post('/resetDb', async (req, res) => {
-  try {
-    await db.query('CALL sp_LoadGreenscapesDb();');
-    console.log('Database was reset');
-
-    res.redirect('/');
-  } catch (error) {
-    console.error('Error resetting database:', error);
-    res.status(500).send('Failed to reset database.');
-  }
-});
-
-app.post('/appointments/:appointmentId/service', async function (req, res) {
-    try {
-        const { appointmentId } = req.params;
-        let data = req.body;
-
-        if (isNaN(parseInt(data.newServiceId)))
-            data.newServiceId = null;
-
-        const query1 = `CALL sp_InsertAppointmentService(?, ?, @resultStatus);`;
-
-        const [rows] = await db.query(query1, [
-            data.newServiceId,
-            appointmentId
-        ]);
-
-        if (rows.affectedRows == 1)
-            console.log('Service was successfully added');
-        else 
-            console.log('Service failed to be added');
-
-        res.redirect(`/appointments/${appointmentId}`);
-    } catch (error) {
-        console.error('Error executing queries:', error);
-        res.status(500).send(
-            'An error occurred while executing the database queries.'
-        );
-    }
-});
-
-app.patch('/appointments/:appointmentId/service/:oldServiceId', async function (req, res) {
-    try {
-        const { appointmentId, oldServiceId } = req.params;
-        let data = req.body;
-
-        if (isNaN(parseInt(data.newServiceId)))
-            data.newServiceId = null;
-
-        const query1 = 'CALL sp_UpdateAppointmentService(?, ?, ?, @resultStatus);';
-        const [rows] = await db.query(query1, [
-            oldServiceId,
-            data.newServiceId,
-            appointmentId
-        ]);
-        
-        if (rows.affectedRows == 1)
-            console.log(`Service was successfully updated `);
-        else 
-            console.log(`Service failed to be updated`);
-    } catch (error) {
-        console.error('Error executing queries:', error);
-        res.status(500).send(
-            'An error occurred while executing the database queries.'
-        );
-    }
-});
-
-app.delete('/appointments/:appointmentId/services/:serviceId', async function (req, res) {
-    try {
-        const { appointmentId, serviceId } = req.params;
-
-        const query1 = `CALL sp_DeleteAppointmentService(?, ?, @resultStatus);`;
-        const [result] = await db.query(query1, [
-            parseInt(appointmentId),
-            parseInt(serviceId)
-        ]);
-
-        if (result.affectedRows == 1)
-            res.status(200).send('Service was successfully deleted');
-        else 
-            res.status(500).send('Service failed to be deleted');
-        
-    } catch (error) {
-        console.error('Error executing queries:', error);
-        // Send a generic error message to the browser
-        res.status(500).send(
-            'An error occurred while executing the database queries.'
-        );
-    }
-});
+app.post('/appointments/:appointmentId/service', handle.addAppointmentService);
+app.patch('/appointments/:appointmentId/service/:oldServiceId', handle.updateAppointmentService);
+app.delete('/appointments/:appointmentId/services/:serviceId', );
 
 app.use('/static', express.static('static'));
 
---
+++ web/handlers.js
@@ -0,0 +1,170 @@
+function getRootHTML(_, res) {
+    res.status(200).render('index');
+}
+
+async function getEmployeesHTML(_, res) {
+    const [employees] = await db.execute('SELECT * FROM Employees;');
+    res.status(200).render('employees', { employees });
+}
+
+async function getServicesHTML(_, res) {
+    const [services] = await db.execute('SELECT * FROM Services;');
+    res.status(200).render('services', { services });
+}
+
+async function getCustomersHTML(_, res) {
+    const [customers] = await db.execute('SELECT * FROM Customers;');
+    res.status(200).render('customers', { customers });
+}
+
+async function getInvoicesHTML(_, res) {
+    const [invoices] = await db.execute('SELECT * FROM Invoices;');
+    res.status(200).render('invoices', { invoices });
+}
+
+async function getAppointmentsHTML(_, res) {
+    const [appointments] = await db.execute(`
+        SELECT 
+            a.appointmentId,
+            e.name AS employeeName,
+            c.name AS customerName,
+            a.appointmentDate,
+            a.status,
+            a.notes,
+            GROUP_CONCAT(s.name ORDER BY s.name SEPARATOR ', ') AS servicesRequested
+        FROM Appointments a
+            JOIN Employees e ON a.employeeId = e.employeeId
+            JOIN Customers c ON a.customerId = c.customerId
+            LEFT JOIN AppointmentServices aps ON a.appointmentId = aps.appointmentId
+            LEFT JOIN Services s ON aps.serviceId = s.serviceId
+        GROUP BY a.appointmentId, e.name, c.name, a.appointmentDate, a.status, a.notes
+        ORDER BY a.appointmentDate ASC;
+    `);
+
+    res.status(200).render('appointments', { appointments });
+}
+
+async function getNewAppointmentHTML(_, res) {
+    const [employees] = await db.execute('SELECT * FROM Employees;');
+    const [customers] = await db.execute('SELECT * FROM Customers;');
+    const [services] = await db.execute('SELECT * FROM Services;');
+
+    res.status(200).render('appointment_input', { employees, customers, services, newAppointment: true });
+}
+
+async function getEditAppointmentHTML (req, res) {
+    const [employees] = await db.execute('SELECT * FROM Employees;');
+    const [customers] = await db.execute('SELECT * FROM Customers;');
+    const [services] = await db.execute('SELECT * FROM Services;');
+
+    const [[appointment]] = await db.execute('SELECT * FROM Appointments WHERE appointmentId = ?;', [req.params.id]);
+    const [selectedServices] = await db.execute('SELECT serviceId FROM AppointmentServices WHERE appointmentId = ?;', [req.params.id]);
+    res.status(200).render('appointment_input', { employees, customers, services, appointment, selectedServices, newAppointment: false });
+}
+
+async function resetDb(_, res) {
+  try {
+    await db.query('CALL sp_LoadGreenscapesDb();');
+    console.log('Database was reset');
+
+    res.redirect('/');
+  } catch (error) {
+    console.error('Error resetting database:', error);
+    res.status(500).send('Failed to reset database.');
+  }
+}
+
+async function addAppointmentService (req, res) {
+    try {
+        const { appointmentId } = req.params;
+        let data = req.body;
+
+        if (isNaN(parseInt(data.newServiceId)))
+            data.newServiceId = null;
+
+        const query1 = `CALL sp_InsertAppointmentService(?, ?, @resultStatus);`;
+
+        const [rows] = await db.query(query1, [
+            data.newServiceId,
+            appointmentId
+        ]);
+
+        if (rows.affectedRows == 1)
+            console.log('Service was successfully added');
+        else 
+            console.log('Service failed to be added');
+
+        res.redirect(`/appointments/${appointmentId}`);
+    } catch (error) {
+        console.error('Error executing queries:', error);
+        res.status(500).send(
+            'An error occurred while executing the database queries.'
+        );
+    }
+}
+
+async function updateAppointmentService(req, res) {
+    try {
+        const { appointmentId, oldServiceId } = req.params;
+        let data = req.body;
+
+        if (isNaN(parseInt(data.newServiceId)))
+            data.newServiceId = null;
+
+        const query1 = 'CALL sp_UpdateAppointmentService(?, ?, ?, @resultStatus);';
+        const [rows] = await db.query(query1, [
+            oldServiceId,
+            data.newServiceId,
+            appointmentId
+        ]);
+        
+        if (rows.affectedRows == 1)
+            console.log(`Service was successfully updated `);
+        else 
+            console.log(`Service failed to be updated`);
+    } catch (error) {
+        console.error('Error executing queries:', error);
+        res.status(500).send(
+            'An error occurred while executing the database queries.'
+        );
+    }
+}
+
+async function deleteAppointmentService(req, res) {
+    try {
+        const { appointmentId, serviceId } = req.params;
+
+        const query1 = `CALL sp_DeleteAppointmentService(?, ?, @resultStatus);`;
+        const [result] = await db.query(query1, [
+            parseInt(appointmentId),
+            parseInt(serviceId)
+        ]);
+
+        if (result.affectedRows == 1)
+            res.status(200).send('Service was successfully deleted');
+        else 
+            res.status(500).send('Service failed to be deleted');
+        
+    } catch (error) {
+        console.error('Error executing queries:', error);
+        // Send a generic error message to the browser
+        res.status(500).send(
+            'An error occurred while executing the database queries.'
+        );
+    }
+}
+
+module.exports = {
+    getRootHTML,
+    getEmployeesHTML,
+    getServicesHTML,
+    getCustomersHTML,
+    getInvoicesHTML,
+    getAppointmentsHTML,
+    getNewAppointmentHTML,
+    getEditAppointmentHTML,
+    resetDb,
+    addAppointmentService,
+    updateAppointmentService,
+    deleteAppointmentService
+}
